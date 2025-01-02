81d1d5e20ff3cce5fe0ee6cb84b029cdc4d08926
Ivan_Enko <110199ws@gmail.com>
2025-01-02 17:51:14 +0200

    fixed task-3

--- js/task-3.js
+++ js/task-3.js
@@ -15,6 +15,5 @@ function toNumber(property) {
 
 // Examples
 console.log(getElementWidth("50px", "8px", "4px")); // 74
-("50px", "8px", "4px")
 console.log(getElementWidth("60px", "12px", "8.5px")); // 101
 console.log(getElementWidth("200px", "0px", "0px")); // 200
