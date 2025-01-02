97f6c2276c829b44d47d1526e688322a1e075cc6
Ivan_Enko <110199ws@gmail.com>
2025-01-02 17:49:11 +0200

    task done

---
+++ js/task-3.js
@@ -0,0 +1,20 @@
+'use strict';
+
+function getElementWidth(content, padding, border) {
+  const totalWidth =
+    toNumber(content) +
+    (toNumber(padding) * 2) +
+    (toNumber(border) * 2);
+
+  return totalWidth;
+}
+
+function toNumber(property) {
+  return Number.parseFloat(property);
+}
+
+// Examples
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+("50px", "8px", "4px")
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
